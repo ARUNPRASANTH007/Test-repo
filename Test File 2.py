905382a75ea7c0aa914ae604626df465fd3ca002
Arunprasanth <arunp@LAPTOP-2P6PH8FL.(none)>
2021-01-21 12:04:14 +0530

    added new feature - counting of defective bottles

--- Test File 2.py
+++ Test File 2.py
@@ -1,9 +1,21 @@
 # Bottle defect type
-detect = input('Enter crack / shape / dust: ')
-if detect == 'crack':
-    print('A crack is detected on a bottle')
-elif detect == 'shape':
-    print('The bottle is not in perfect shape')
-elif detect == 'dust':
-    print('There is a foreign particle in the bottle')
-    
+no_of_defective_bottles = 0
+detect = input('Enter crack / shape / dust / no: ')
+
+while True:
+    no_of_defective_bottles += 1
+    if detect == 'crack':
+        print('A crack is detected on a bottle')
+    elif detect == 'shape':
+        print('The bottle is not in perfect shape')
+    elif detect == 'dust':
+        print('There is a foreign particle in the bottle')
+    elif detect == 'no':
+        no_of_defective_bottles -= 1
+        print('Bottle is ready for filling')
+    else:
+        print('Invalid input')
+        break
+
+#counting the defective bottles
+print('No. of defective bottles', no_of_defective_bottles)
