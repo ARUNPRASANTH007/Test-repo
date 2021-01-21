31aed57c37404243daca9682722b52fd2f6c5ac8
Arunprasanth <arunp@LAPTOP-2P6PH8FL.(none)>
2021-01-21 11:01:35 +0530

    another version is reverted

--- Test File 2.py
+++
@@ -1,8 +0,0 @@
-# Bottle defect type
-detect = input('Enter crack / shape / dust: ')
-if detect == 'crack':
-    print('A crack is detected on a bottle')
-elif detect == 'shape':
-    print('The bottle is not in perfect shape')
-elif detect == 'dust':
-    print('There is a foreign particle in the bottle')
